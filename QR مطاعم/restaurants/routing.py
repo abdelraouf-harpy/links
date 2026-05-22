630c470fb4deb5962c450908a555c084edb2ec0d
Abdel Raouf Ahmed <abdomrx81@gmail.com>
2026-05-22 21:16:50 +0300

    Delete QR مطاعم directory

--- QR مطاعم/restaurants/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/kitchen/<int:restaurant_id>/', consumers.OrderConsumer.as_asgi()),
-    path('ws/notifications/<int:restaurant_id>/', consumers.NotificationConsumer.as_asgi()),
-    path('ws/order-status/<uuid:table_token>/', consumers.CustomerOrderConsumer.as_asgi()),
-]
