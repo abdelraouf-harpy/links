b70611d7f1055892712834639b486a8ce2abd696
Abdel Raouf Ahmed <abdomrx81@gmail.com>
2026-09-01 21:35:40 +0300

    Add files via upload

--- order/sw.js
+++ order/sw.js
@@ -1,5 +1,5 @@
 // HarpyOrder Service Worker — Dynamic Fast-Update & Offline Engine
-const CACHE_NAME = 'harpy-order-v8.0';
+const CACHE_NAME = 'harpy-order-v8.1';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
